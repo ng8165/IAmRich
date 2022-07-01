29ce7aa83470c5a140cb6b7e96a7d215b08a41e2
Nelson Gou <nel4gou@gmail.com>
2022-06-30 18:37:22 -0700

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Nelson Gou on 6/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
